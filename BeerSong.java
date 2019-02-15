731f24a72b1040b33aa112406a2c9af676216802
XhoniK <xhoni.krifsa@gmail.com>
2019-02-15 08:56:14 +0530

    Add BeerSong interface

---
+++ BeerSong.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beersong;
+
+/**
+ *
+ * @author vorce
+ */
+public class BeerSong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    public static void Ninety_Nine_Bottles_of_Beer() {
+      
+    }
+}
